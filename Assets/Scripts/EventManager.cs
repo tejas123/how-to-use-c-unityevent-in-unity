986046856cf26e683e08f93caf15ec136a4e21af
Tejas Jasani <bharatappguru@gmail.com>
2018-07-06 12:10:44 +0530

    Initial Commit : Blog : how-to-use-c-unityevent-in-unity Author ; Bansi Vithlani

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour {
+
+	#region PUBLIC_VARIABLES
+	public static EventManager Instance;
+	public UnityEvent testEvent;
+	#endregion
+
+	#region UNITY_CALLBACKS
+	void Awake(){
+		Instance = this;
+	}
+	void Start(){
+		StartCoroutine (TestEventDemo() );
+	}
+	#endregion
+
+	#region CO_ROUTINES
+	IEnumerator TestEventDemo(){
+		yield return new WaitForSeconds(1f);
+		testEvent.Invoke ();
+	}
+	#endregion
+}
---
+++ Assets/Scripts/UnityEventDemo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityEventDemo : MonoBehaviour {
+	#region PUBLIC_VERIABLE
+	public Text demoText;
+	#endregion
+	#region PRIVATE_VARIABLE
+	int initialSize=10;
+	int maxSize=100 ;
+	#endregion
+	#region UNITY_CALLBACKS
+	void start(){
+	}
+	#endregion
+	#region PUBLIC_VARIABLE
+	public void TextChange(){
+		StartCoroutine (WaitAndChangeText (1));
+	}
+	#endregion
+	#region CO_ROUTINE
+	IEnumerator WaitAndChangeText(float time){
+		float i = 0;
+		float rate = 1 / time;
+		while (i < 1) {
+			i += Time.deltaTime * rate;
+			demoText.resizeTextMaxSize = (int)Mathf.Lerp (initialSize, maxSize, i);
+			yield return new WaitForSeconds (0.01f);
+		}
+	}
+	#endregion
+
+}
